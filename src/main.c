1bf4e4b6863cbf2e460b6813a38bcfedd21cd3b4
yaye <viktorastankus37@gmail.com>
2026-02-10 21:18:26 +0200

    opening current directory

--- src/main.c
+++ src/main.c
@@ -1,6 +1,18 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
 
-int main(void){
-	system("xdg-open");
+int main(int argc, char *argv[]){
+	if(argc < 2){
+		printf("Usage: $live-viewer <HTML FILE NAME> \n");
+		return 1;
+	}
+
+	char current_directory[4096];
+	getcwd(current_directory, sizeof(current_directory));
+	char command[5016];
+	snprintf(command, sizeof(command), "xdg-open %s/%s", current_directory, argv[1]);
+	system(command);
 	return 0;
 }
