c43eb41fe324ab41b96cd406bfbf06249d57d4e8
yaye <viktorastankus37@gmail.com>
2026-02-10 21:40:18 +0200

    command line args

--- src/main.c
+++ src/main.c
@@ -1,13 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <limits.h>
+#include <string.h>
 
 int main(int argc, char *argv[]){
 	if(argc < 2){
 		printf("Usage: $live-viewer <HTML FILE NAME> \n");
+		printf("       $live-viewer -help for available commands \n\n");
 		return 1;
 	}
+
+	if(strcmp("-help", argv[1]) == 0){
+		printf("FLAGS:       -help              <lists all commands> \n");
+		printf("             -port PORT_NUMBER  <changes port> \n\n");
+		printf("COMMANDS:    stop               <stops the process> \n");
+		return 1;
+	}
+	
 
 	char current_directory[4096];
 	getcwd(current_directory, sizeof(current_directory));
